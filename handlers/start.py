51a3d5ddb8233f1ac300156322ba459cfd54297d
mrismanaziz <mrismanaziz@gmail.com>
2021-04-21 10:06:42 +0700

    Music-Man

--- handlers/start.py
+++ handlers/start.py
@@ -6,12 +6,12 @@ from config import BOT_NAME as bn
 @Client.on_message(filters.command("start") & filters.private & ~filters.channel)
 async def start(_, message: Message):
     await message.reply_text(
-        f"""Halo 👋! Saya dapat memutar musik dalam obrolan suara Grup Telegeam.\n\n✣ Apakah Anda ingin saya memutar musik di obrolan suara grup Telegram Anda? Silakan klik \'📜 Panduan Menggunakan BOT 📜\' tombol di bawah untuk mengetahui bagaimana cara menggunakan saya.\n\n✣ Tambahkan [Assistant Music Man](https://t.me/botmusikman) ke grup Anda untuk memutar musik di obrolan suara grup Anda.\n\n✣ Info & perintah selengkapnya yang disebutkan di [User Manual](https://telegra.ph/BOT-Music-Man-Voice-Chat-Group-04-16)\n\nManaged With ☕️ By [Risman](https://t.me/mrismanaziz)""",
+        f"""Halo 👋! Saya dapat memutar musik dalam obrolan suara Grup Telegram.\n\n✣ Apakah Anda ingin saya memutar musik di obrolan suara grup Telegram Anda? Silakan klik \'📜 Panduan Menggunakan BOT 📜\' tombol di bawah untuk mengetahui bagaimana cara menggunakan saya.\n\n✣ Tambahkan [Assistant Music Man](https://t.me/botmusikman) ke grup Anda untuk memutar musik di obrolan suara grup Anda.\n\n✣ Info & perintah selengkapnya yang disebutkan di [User Manual](https://telegra.ph/BOT-Music-Man-Voice-Chat-Group-04-16)\n\nManaged With ☕️ By [Risman](https://t.me/mrismanaziz)""",
         reply_markup=InlineKeyboardMarkup(
             [ 
                 [
                     InlineKeyboardButton(
-                        "📜 Panduan Menggunakan BOT 📜", url="https://telegra.ph/BOT-Music-Man-Voice-Chat-Group-04-16")
+                        "📜 Panduan Menggunakan BOT 📜", url="https://t.me/Lunatic0de/20")
                   ],[
                     InlineKeyboardButton(
                         "Group Support", url="https://t.me/SharingUserbot"
@@ -35,7 +35,7 @@ async def gstart(_, message: Message):
                         "Group Support", url="https://t.me/SharingUserbot"
                     ),
                     InlineKeyboardButton(
-                        "Owner", url="https://t.com/mrismanaziz"
+                        "Owner", url="https://t.me/mrismanaziz"
                     )
                 ]
             ]
@@ -44,7 +44,7 @@ async def gstart(_, message: Message):
 
 @Client.on_message(filters.command("reload") & ~filters.private & ~filters.channel)
 async def gstart(_, message: Message):
-      await message.reply_text("""**✣ Pemutar Musik Sedang Online ✣**""",
+      await message.reply_text("""✅ **Pemutar Musik Sedang Online **""",
       reply_markup=InlineKeyboardMarkup(
             [
                 [
@@ -52,7 +52,7 @@ async def gstart(_, message: Message):
                         "Group Support", url="https://t.me/SharingUserbot"
                     ),
                     InlineKeyboardButton(
-                        "Owner", url="https://t.com/mrismanaziz"
+                        "Owner", url="https://t.me/mrismanaziz"
                     )
                 ]
             ]
