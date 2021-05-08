fea829e34f6b574d31217e96846f5edde15577e8
Muhamad Risman Aziz <62795826+mrismanaziz@users.noreply.github.com>
2021-05-09 02:52:28 +0700

    ytsearch: beautify

--- handlers/ytsearch.py
+++ handlers/ytsearch.py
@@ -46,11 +46,12 @@ async def ytsearch(_, message: Message):
         i = 0
         text = ""
         while i < 4:
-            text += f"Title - {results[i]['title']}\n"
-            text += f"Duration - {results[i]['duration']}\n"
-            text += f"Views - {results[i]['views']}\n"
-            text += f"Channel - {results[i]['channel']}\n"
-            text += f"https://youtube.com{results[i]['url_suffix']}\n\n"
+            text += f"**Judul Lagu :** {results[i]['title']}\n"
+            text += f"**Durasi :** {results[i]['duration']}\n"
+            text += f"**Dilihat :** {results[i]['views']}\n"
+            text += f"**Channel :** {results[i]['channel']}\n"
+            text += f"https://youtube.com{results[i]['url_suffix']}\n"
+            text += "━━\n"
             i += 1
         await m.edit(text, disable_web_page_preview=True)
     except Exception as e:
