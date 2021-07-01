c7c1c49c19f25581ca0a04d032f1faa0206a1573
mrismanaziz <mrismanaziz@gmail.com>
2021-07-01 19:39:18 +0700

    Music-Man: update

--- MusicMan/modules/private.py
+++ MusicMan/modules/private.py
@@ -30,6 +30,116 @@ from MusicMan.config import BOT_USERNAME
 from MusicMan.config import OWNER
 logging.basicConfig(level=logging.INFO)
 
+
+
+@Client.on_message(
+    filters.command("start")
+    & filters.private
+    & ~ filters.edited
+)
+async def start_(client: Client, message: Message):
+    await message.reply_text(
+        f"""<b>┗┓ Haii {message.from_user.first_name} saya adalah {PROJECT_NAME} ┏┛\n
+Saya Bot Music Group, Yang dapat Memutar Lagu di Voice Chat Group Dengan cara yang Mudah
+Saya Memiliki Banyak Fitur Praktis Seperti :
+┏━━━━━━━━━━━━━━
+┣• Memutar Musik.
+┣• Mendownload Lagu.
+┣• Mencari Lagu Yang ingin di Putar atau di Download.
+┣• Gunakan Perintah » /help « untuk Mengetahui Fitur Lengkap saya
+┗━━━━━━━━━━━━━━
+❃ Managed With ❤ By {OWNER}
+❃ Thanks To [Risman](https://t.me/mrismanaziz)
+━━━━━━━━━━━━━━━
+Ingin Menambahkan Saya ke Grup Anda? Tambahkan Saya Ke Group Anda!
+</b>""",
+
+# Edit Yang Perlu Lu ganti 
+# Tapi Jangan di Hapus Thanks To nya Yaaa :D
+
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "➕ Tambahkan saya ke Grup Anda ➕", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")],
+                [
+                    InlineKeyboardButton(
+                        "💬 Channel Updates", url=f"https://t.me/{UPDATES_CHANNEL}"), 
+                    InlineKeyboardButton(
+                        "⛑ Group Support", url=f"https://t.me/{SUPPORT_GROUP}")
+                ],[
+                    InlineKeyboardButton(
+                        "🛠 Source Code 🛠", url=f"https://{SOURCE_CODE}")
+                ]
+            ]
+        ),
+        reply_to_message_id=message.message_id
+        )
+
+@Client.on_message(filters.private & filters.incoming & filters.command(['help']))
+def _help(client, message):
+    client.send_message(chat_id = message.chat.id,
+        text = tr.HELP_MSG[1],
+        parse_mode="markdown",
+        disable_web_page_preview=True,
+        disable_notification=True,
+        reply_markup = InlineKeyboardMarkup(map(1)),
+        reply_to_message_id = message.message_id
+    )
+
+help_callback_filter = filters.create(lambda _, __, query: query.data.startswith('help+'))
+
+@Client.on_callback_query(help_callback_filter)
+def help_answer(client, callback_query):
+    chat_id = callback_query.from_user.id
+    disable_web_page_preview=True
+    message_id = callback_query.message.message_id
+    msg = int(callback_query.data.split('+')[1])
+    client.edit_message_text(chat_id=chat_id,    message_id=message_id,
+        text=tr.HELP_MSG[msg],    reply_markup=InlineKeyboardMarkup(map(msg))
+    )
+
+
+def map(pos):
+    if (pos==1):
+        return [
+            [InlineKeyboardButton(text = 'Next »', callback_data = "help+2")]
+        ]
+    elif pos==len(tr.HELP_MSG)-1:
+        url = f"https://t.me/{SUPPORT_GROUP}"
+        return [
+            [
+                InlineKeyboardButton(
+                    "➕ Tambahkan saya ke Grup Anda ➕",
+                    url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+                )
+            ],
+            [
+                InlineKeyboardButton(
+                    text='💬 Channel Updates',
+                    url=f"https://t.me/{UPDATES_CHANNEL}",
+                ),
+                InlineKeyboardButton(
+                    text='⛑ Group Support', url=f"https://t.me/{SUPPORT_GROUP}"
+                ),
+            ],
+            [
+                InlineKeyboardButton(
+                    text='🛠 Source Code 🛠', url=f"https://{SOURCE_CODE}"
+                )
+            ],
+            [InlineKeyboardButton(text='«', callback_data=f"help+{pos-1}")],
+        ]
+
+    else:
+        return [
+            [
+                InlineKeyboardButton(text = '«', callback_data = f"help+{pos-1}"),
+                InlineKeyboardButton(text = '»', callback_data = f"help+{pos+1}")
+            ],
+        ]
+
+
 @Client.on_message(
     filters.command("start")
     & filters.group
