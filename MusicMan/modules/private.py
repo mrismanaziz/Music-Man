706fa773f055521ec4491916bbc3181fd3ddeefd
Restyled.io <commits@restyled.io>
2021-07-18 06:51:34 +0000

    Restyled by black

--- MusicMan/modules/private.py
+++ MusicMan/modules/private.py
@@ -28,14 +28,11 @@ from MusicMan.config import SUPPORT_GROUP
 from MusicMan.config import UPDATES_CHANNEL
 from MusicMan.config import BOT_USERNAME
 from MusicMan.config import OWNER
+
 logging.basicConfig(level=logging.INFO)
 
 
-@Client.on_message(
-    filters.command("start")
-    & filters.private
-    & ~ filters.edited
-)
+@Client.on_message(filters.command("start") & filters.private & ~filters.edited)
 async def start_(client: Client, message: Message):
     await message.reply_text(
         f"""<b>┗┓ Haii {message.from_user.first_name} saya adalah {PROJECT_NAME} ┏┛\n
@@ -52,44 +49,47 @@ Saya Memiliki Banyak Fitur Praktis Seperti :
 ━━━━━━━━━━━━━━━
 Ingin Menambahkan Saya ke Grup Anda? Tambahkan Saya Ke Group Anda!
 </b>""",
-
         # Edit Yang Perlu Lu ganti
         # Tapi Jangan di Hapus Thanks To nya Yaaa :D
-
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "➕ Tambahkan saya ke Grup Anda ➕", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")],
+                        "➕ Tambahkan saya ke Grup Anda ➕",
+                        url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+                    )
+                ],
                 [
                     InlineKeyboardButton(
-                        "💬 Channel Updates", url=f"https://t.me/{UPDATES_CHANNEL}"),
+                        "💬 Channel Updates", url=f"https://t.me/{UPDATES_CHANNEL}"
+                    ),
                     InlineKeyboardButton(
-                        "⛑ Group Support", url=f"https://t.me/{SUPPORT_GROUP}")
-                ], [
-                    InlineKeyboardButton(
-                        "🛠 Source Code 🛠", url=f"https://{SOURCE_CODE}")
-                ]
+                        "⛑ Group Support", url=f"https://t.me/{SUPPORT_GROUP}"
+                    ),
+                ],
+                [InlineKeyboardButton("🛠 Source Code 🛠", url=f"https://{SOURCE_CODE}")],
             ]
         ),
-        reply_to_message_id=message.message_id
+        reply_to_message_id=message.message_id,
     )
 
 
-@Client.on_message(filters.private & filters.incoming & filters.command(['help']))
+@Client.on_message(filters.private & filters.incoming & filters.command(["help"]))
 def _help(client, message):
-    client.send_message(chat_id=message.chat.id,
-                        text=tr.HELP_MSG[1],
-                        parse_mode="markdown",
-                        disable_web_page_preview=True,
-                        disable_notification=True,
-                        reply_markup=InlineKeyboardMarkup(map(1)),
-                        reply_to_message_id=message.message_id
-                        )
+    client.send_message(
+        chat_id=message.chat.id,
+        text=tr.HELP_MSG[1],
+        parse_mode="markdown",
+        disable_web_page_preview=True,
+        disable_notification=True,
+        reply_markup=InlineKeyboardMarkup(map(1)),
+        reply_to_message_id=message.message_id,
+    )
 
 
 help_callback_filter = filters.create(
-    lambda _, __, query: query.data.startswith('help+'))
+    lambda _, __, query: query.data.startswith("help+")
+)
 
 
 @Client.on_callback_query(help_callback_filter)
@@ -97,19 +97,19 @@ def help_answer(client, callback_query):
     chat_id = callback_query.from_user.id
     disable_web_page_preview = True
     message_id = callback_query.message.message_id
-    msg = int(callback_query.data.split('+')[1])
-    client.edit_message_text(chat_id=chat_id,    message_id=message_id,
-                             text=tr.HELP_MSG[msg],    reply_markup=InlineKeyboardMarkup(
-                                 map(msg))
-                             )
+    msg = int(callback_query.data.split("+")[1])
+    client.edit_message_text(
+        chat_id=chat_id,
+        message_id=message_id,
+        text=tr.HELP_MSG[msg],
+        reply_markup=InlineKeyboardMarkup(map(msg)),
+    )
 
 
 def map(pos):
-    if (pos == 1):
-        return [
-            [InlineKeyboardButton(text='Next »', callback_data="help+2")]
-        ]
-    elif pos == len(tr.HELP_MSG)-1:
+    if pos == 1:
+        return [[InlineKeyboardButton(text="Next »", callback_data="help+2")]]
+    elif pos == len(tr.HELP_MSG) - 1:
         url = f"https://t.me/{SUPPORT_GROUP}"
         return [
             [
@@ -120,50 +120,42 @@ def map(pos):
             ],
             [
                 InlineKeyboardButton(
-                    text='💬 Channel Updates',
+                    text="💬 Channel Updates",
                     url=f"https://t.me/{UPDATES_CHANNEL}",
                 ),
                 InlineKeyboardButton(
-                    text='⛑ Group Support', url=f"https://t.me/{SUPPORT_GROUP}"
+                    text="⛑ Group Support", url=f"https://t.me/{SUPPORT_GROUP}"
                 ),
             ],
             [
                 InlineKeyboardButton(
-                    text='🛠 Source Code 🛠', url=f"https://{SOURCE_CODE}"
+                    text="🛠 Source Code 🛠", url=f"https://{SOURCE_CODE}"
                 )
             ],
-            [InlineKeyboardButton(text='«', callback_data=f"help+{pos-1}")],
+            [InlineKeyboardButton(text="«", callback_data=f"help+{pos-1}")],
         ]
 
     else:
         return [
             [
-                InlineKeyboardButton(text='«', callback_data=f"help+{pos-1}"),
-                InlineKeyboardButton(text='»', callback_data=f"help+{pos+1}")
+                InlineKeyboardButton(text="«", callback_data=f"help+{pos-1}"),
+                InlineKeyboardButton(text="»", callback_data=f"help+{pos+1}"),
             ],
         ]
 
 
-@Client.on_message(
-    filters.command("start")
-    & filters.group
-    & ~ filters.edited
-)
+@Client.on_message(filters.command("start") & filters.group & ~filters.edited)
 async def start(client: Client, message: Message):
     await message.reply_text(
         "💁🏻‍♂️ **Apakah Anda ingin mencari Link YouTube?**",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton(
-                        "✅ Ya", switch_inline_query_current_chat=""
-                    ),
-                    InlineKeyboardButton(
-                        "❌ Tidak ", callback_data="close"
-                    )
+                    InlineKeyboardButton("✅ Ya", switch_inline_query_current_chat=""),
+                    InlineKeyboardButton("❌ Tidak ", callback_data="close"),
                 ]
             ]
-        )
+        ),
     )
 
 
@@ -175,7 +167,8 @@ async def ghelp(_, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "📜 Cara Menggunakan BOT 📜", url=f"https://t.me/{BOT_USERNAME}?start"
+                        "📜 Cara Menggunakan BOT 📜",
+                        url=f"https://t.me/{BOT_USERNAME}?start",
                     )
                 ]
             ]
