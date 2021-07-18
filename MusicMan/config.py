de9b189969057f4bfa59d2800e90e71fcaae81c6
Restyled.io <commits@restyled.io>
2021-07-18 06:33:24 +0000

    Restyled by reorder-python-imports

--- MusicMan/config.py
+++ MusicMan/config.py
@@ -5,7 +5,6 @@
 # it under the terms of the GNU Affero General Public License as
 # published by the Free Software Foundation, either version 3 of the
 # License, or (at your option) any later version.
-
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -14,7 +13,6 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 # Modified by Inukaasith
-
 import os
 from os import getenv
 
